750e67d80234ccc0ec8399f842ce3ed240d19e67
Tyler Cruz <tymicruz@gmail.com>
2025-07-25 18:25:11 -0400

    Try to fix deploy.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const REPO_NAME = 'https://github.com/tymicruz/mastermind-react-app';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
