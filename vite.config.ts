e104238145e3b0a079975f82d2aa5e7d2af75e3c
Tyler Cruz <tymicruz@gmail.com>
2025-07-25 17:57:51 -0400

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const REPO_NAME = 'https://github.com/tymicruz/mastermind-react-app';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mastermind-react-app/',
+  base: `/${REPO_NAME}`,
 })
